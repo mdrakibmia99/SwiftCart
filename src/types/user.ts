b564b3a447ac9d2a7c4c805a23eccc6a4318f3f7
khaledssbd <khaledssbd@gmail.com>
2025-05-25 16:11:39 +0600

    Update Navbar to use dynamic user profile photo and adjust IUser interface for consistency

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,9 +2,10 @@ export interface IUser {
   userId: string;
   name: string;
   email: string;
+  profilePhoto?: string;
   hasShop?: boolean;
   isActive?: boolean;
-  role: "user" | "admin";
+  role: 'user' | 'admin';
   iat?: number;
   exp?: number;
 }
